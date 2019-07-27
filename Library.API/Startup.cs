2201e3dda564b70d6ec0535278df3e4ab9dd3d87
MIzu Rahman <dhmizu95@gmail.com>
2019-07-27 06:39:17 +0600

    Add project files.

---
+++ Library.API/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Library.API.Entities;
+using Library.API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Library.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<LibraryContext>(opt =>
+                opt.UseMySql((Configuration.GetConnectionString("DefaultConnection"))));
+
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            LibraryContext libraryContext, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            libraryContext.EnsureSeedDataForContext();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
